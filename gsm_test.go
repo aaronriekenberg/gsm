b2c05889b2a76dc4fb8c797e944cbf0e80a2da6e
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-03-15 16:11:56 -0500

    Add benchmarks.

--- gsm_test.go
+++ gsm_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"maps"
 	"slices"
+	"sync"
 	"testing"
 
 	"github.com/aaronriekenberg/gsm"
@@ -51,6 +52,46 @@ func ExampleGenericSyncMap() {
 	// bob age = 35
 	// swapped = true value = {title:manager age:25} ok = true
 	// keys = [alice bob]
+}
+
+func BenchmarkSyncMapParallelLoad(b *testing.B) {
+	var m sync.Map
+
+	m.Store(1, "one")
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			m.Load(1)
+		}
+	})
+}
+
+func BenchmarkGenericSyncMapParallelLoad(b *testing.B) {
+	var m gsm.GenericSyncMap[int, string]
+
+	m.Store(1, "one")
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			m.Load(1)
+		}
+	})
+}
+
+func BenchmarkRWMutexMapParallelLoad(b *testing.B) {
+	m := make(map[int]string)
+	rwm := sync.RWMutex{}
+
+	m[1] = "one"
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			rwm.RLock()
+			defer rwm.RUnlock()
+
+			_ = m[1]
+		}
+	})
 }
 
 func TestLoad(t *testing.T) {
